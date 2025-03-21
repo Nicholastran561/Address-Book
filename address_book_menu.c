0383dd8819fc724d79027aed7187d49047f6cbcc
Gabriel Fernandez <grfernandez@cpp.edu>
2025-03-20 22:10:30 -0700

    Version 1.0.0

--- address_book_menu.c
+++ address_book_menu.c
@@ -196,10 +196,111 @@ Status menu(AddressBook *address_book)
 	return e_success;
 }
 
+Status getContactInfo(ContactInfo *wList, const int *_si_no) {
+	int amountOfNumbers;
+	char numbs[10];
+	char eNumbs[10];
+
+	char *ptr;
+	char *ePtr;
+
+	printf("\n\n### Adding data to contact list. ###");
+
+	//fflush(stdin);
+	printf("\nWhat is the contact's name?: ");
+	scanf("%s", wList->name);//, sizeof(wList->name), stdin);
+	
+	//scanf("%s", wList->name);
+	printf("%s", wList->name);
+
+	//Phone numbers
+	do
+	{
+		printf("\nHow many phone numbers does the contact have? (Max numbers: %i): ", MAX_PHONE_NUMBERS);
+		scanf("%i", &amountOfNumbers);//fgets(numbs, sizeof(numbs), stdin);
+		//amountOfNumbers = strtol(numbs, &ptr, 10);
+
+	} while (amountOfNumbers > MAX_PHONE_NUMBERS);
+
+	int i = 0;
+	for (; i < amountOfNumbers; ++i)
+	{
+		printf("\nEnter number %i of %s's numbers.: ", i, wList->name);
+		//fgets(wList->phone_numbers[i], sizeof(wList->phone_numbers[i]), stdin);
+		scanf("%s", wList->phone_numbers[i]);
+	}
+
+	//Email addresses
+	do
+	{
+		printf("\nHow many email addresses does the contact have? (Max numbers: %i): ", MAX_EMAILS);
+		scanf("%i", &amountOfNumbers);
+		//fgets(eNumbs, sizeof(eNumbs), stdin);
+		//amountOfNumbers = strtol(eNumbs, &ePtr, 10);
+	} while (amountOfNumbers > MAX_EMAILS);
+
+	i = 0;
+	for (; i < amountOfNumbers; ++i)
+	{
+		printf("\nEnter number %i of %s's emails.: ", i, wList->name);
+		//fgets(wList->email_addresses[i], sizeof(wList->email_addresses[i]), stdin);
+		scanf("%s", wList->email_addresses[i]);
+	}
+
+	wList->si_no = *_si_no;
+
+	return e_success;
+}
+
 Status add_contacts(AddressBook *address_book)
 {
 	menu_header("Add Contacts:\n");
-	/* Add the functionality for adding contacts here */
+	
+	printf("\nFirst allocating memory.");
+
+	int lCount = address_book->count;
+	int cCount = lCount; //TO compare;
+	ContactInfo *nList = address_book->list; //Not sure if malloc fails it will overwrite the data.
+	ContactInfo *nPointer;
+
+	lCount += 1;
+	address_book->count = lCount;
+
+	//If there are no contacts with in the book at this moment.
+	if (cCount == 0)
+	{
+		//Allocate memory within the list.
+		nList = (ContactInfo*) malloc(lCount * sizeof(ContactInfo));
+
+		if (nList == NULL) //Failed
+		{
+			printf("\nFailed to add the first person to contacts. Not enough memory.\n");
+			return e_fail;
+		}
+		else //Success
+		{
+			nPointer = (nList + cCount);
+			getContactInfo(nPointer, &lCount);
+		}
+	}
+	else
+	{
+		nList = (ContactInfo*) realloc(nList, lCount * sizeof(ContactInfo)); //Reallocate enough memory in storage;
+
+		if (nList == NULL)
+		{
+			printf("\nFailed to add the person to contacts. Not enough memory.\n");
+			return e_fail;
+		}
+		else //Add
+		{
+			nPointer = (nList + cCount);
+			getContactInfo(nPointer, &lCount);
+		}
+	}
+
+	address_book->list = nList;
+	
 	char input[MAX_INPUT_LENGTH];
 	while(strcmp(input, "q") != 0) 
 	{
@@ -208,7 +309,6 @@ Status add_contacts(AddressBook *address_book)
 	}
 	return e_success;
 }
-
 Status search(const char *str, AddressBook *address_book, int loop_count, int field, const char *msg, Modes mode)
 {
 	if (address_book->count == 0)
